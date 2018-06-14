347143d87e87107f7cfd21ce078a4d04ed26d3f8
unknown <m.ibarboure@hotmail.fr>
2018-06-14 10:42:01 +0200

    Adding click on inscription button

--- app/src/main/java/com/example/miren/projectin/Inscription_dev.java
+++ app/src/main/java/com/example/miren/projectin/Inscription_dev.java
@@ -1,11 +1,13 @@
 package com.example.miren.projectin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Inscription_dev extends AppCompatActivity {
 
@@ -13,6 +15,13 @@ public class Inscription_dev extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription_dev);
-    }
+        Button inscription_dev = (Button) findViewById(R.id.button3);
 
+        inscription_dev.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent appel_dev = new Intent(Inscription_dev.this, LoginActivity.class);
+                startActivity(appel_dev);
+            }
+        });
+    }
 }
